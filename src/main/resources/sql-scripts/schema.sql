99c122ed7b9c8ed94bb0ad4622321f6078b95e98
root <root@localhost>
2025-03-12 01:04:40 +0000

    WIP

--- src/main/resources/sql-scripts/schema.sql
+++ src/main/resources/sql-scripts/schema.sql
@@ -1,30 +1,25 @@
 CREATE TABLE random_city (
-  id bigint(20) NOT NULL AUTO_INCREMENT,
-  name varchar(255) DEFAULT NULL,
-  PRIMARY KEY (id)
+  id IDENTITY NOT NULL PRIMARY KEY,
+  name varchar(255) DEFAULT NULL
 );
 
 CREATE TABLE app_role (
-  id bigint(20) NOT NULL AUTO_INCREMENT,
+  id IDENTITY NOT NULL PRIMARY KEY,
   description varchar(255) DEFAULT NULL,
-  role_name varchar(255) DEFAULT NULL,
-  PRIMARY KEY (id)
+  role_name varchar(255) DEFAULT NULL
 );
 
-
 CREATE TABLE app_user (
-  id bigint(20) NOT NULL AUTO_INCREMENT,
+  id IDENTITY NOT NULL PRIMARY KEY,
   first_name varchar(255) NOT NULL,
   last_name varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
-  username varchar(255) NOT NULL,
-  PRIMARY KEY (id)
+  username varchar(255) NOT NULL
 );
 
-
 CREATE TABLE user_role (
-  user_id bigint(20) NOT NULL,
-  role_id bigint(20) NOT NULL,
+  user_id bigint NOT NULL,
+  role_id bigint NOT NULL,
   CONSTRAINT FK859n2jvi8ivhui0rl0esws6o FOREIGN KEY (user_id) REFERENCES app_user (id),
   CONSTRAINT FKa68196081fvovjhkek5m97n3y FOREIGN KEY (role_id) REFERENCES app_role (id)
 );
